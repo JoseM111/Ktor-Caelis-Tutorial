72bc8c3259e8fead01ab390d96e248926101bd4d
Jose Martinez <alias_one11@outlook.com>
2021-09-29 23:29:04 -0400

    3. finishing up repositories
    Took 2 hours 30 minutes

---
+++ src/main/kotlin/com/alias111/entities/TodoDraftEntity.kt
@@ -0,0 +1,9 @@
+/** TodoDraftEntity.kt */
+package com.alias111.entities
+// ⚫️⚫️☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰
+
+data class TodoDraftEntity(
+    val title: String,
+    val isDone: Boolean,
+)
+// ⚫️⚫️☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰
--- src/main/kotlin/com/alias111/entities/TodoEntity.kt
+++ src/main/kotlin/com/alias111/entities/TodoEntity.kt
@@ -1,20 +1,13 @@
-/** Todo.kt */
-package com.alias111.models
+/** TodoEntity.kt */
+package com.alias111.entities
 
 //import kotlinx.serialization.*
 // ⚫️⚫️☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰
 
 //@Serializable
-data class Todo(
+data class TodoEntity(
     val id: Int,
     var title: String,
-    var done: Boolean,
-)
-
-val TODO_DUMMY_DATA: List<Todo> = listOf<Todo>(
-    Todo(id = 1, title = "Code", done = true),
-    Todo(id = 2, title = "Drive uber", done = false),
-    Todo(id = 3, title = "Call Wife", done = false),
-    Todo(id = 4, title = "Go to bed early", done = false),
+    var isDone: Boolean,
 )
 // ⚫️⚫️☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰
